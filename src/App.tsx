41bc30fc02ad94175c26b23e67a1d8bb85deab65
Jasaman Singh Chaggar <jsc.21905@gmail.com>
2026-01-19 16:14:48 +0530

    wip: preloader animation added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import HeroSection from '@/components/sections/HeroSection';
 import AboutSection from '@/components/sections/AboutSection';
 import ProjectsSection from '@/components/sections/ProjectsSection';
 import ContactSection from '@/components/sections/ContactSection';
-import Preloader from './components/Preloader';
+import Preloader from '@/components/Preloader';
 
 const App = () => {
   return (
